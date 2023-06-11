beace1c60a27b8ec186b8619473da02e9bdcfcfe
cresb-49 <66991258+cresb-49@users.noreply.github.com>
2023-06-10 22:32:39 -0600

    retorno de valores funcion

--- backend/Instrucciones/retornar.py
+++ backend/Instrucciones/retornar.py
@@ -11,8 +11,12 @@ class Retornar(Abstract):
         return f"Return -> linea: {self.linea} ,columna: {self.columna}"
 
     def ejecutar(self, scope):
-        valor = self.exprecion.ejecutar(scope)
-        return valor
+        if self.exprecion != None:
+            valor = self.exprecion.ejecutar(scope)
+            print('Retorno',valor)
+            return valor
+        else:
+            return None
 
     def graficar(self, graphviz, padre):
         graphviz.add_nodo('return', padre)
