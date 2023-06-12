752cccbe986df5bf2a190d6d5043ad405d31268a
cresb-49 <66991258+cresb-49@users.noreply.github.com>
2023-06-11 22:49:05 -0600

    declaraciones de arreglos

--- backend/Instrucciones/declaracion.py
+++ backend/Instrucciones/declaracion.py
@@ -1,5 +1,6 @@
 from Abstract.abstract import Abstract
 from Symbol.tipoEnum import TipoEnum
+from Expresiones.arreglo import Arreglo
 
 
 class Declaracion(Abstract):
@@ -24,44 +25,107 @@ class Declaracion(Abstract):
             else:
                 result_expresion = {"value": None, "tipo": self.tipo,
                                     "tipo_secundario": None, "linea": self.linea, "columna": self.columna}
-
         if self.tipo == TipoEnum.ANY:
             tipo_secundario: TipoEnum = result_expresion['tipo']
+            #print(f'Declaro variable "{self.id}" con ->',result_expresion)
             try:
-                scope.declarar_variable(self.id, result_expresion['value'], self.tipo, tipo_secundario.value, self.linea, self.columna)
+                scope.declarar_variable(
+                    self.id, result_expresion['value'], self.tipo, tipo_secundario.value, self.linea, self.columna)
             except ValueError as error:
-                self.resultado.add_error('Semantico',str(error),self.linea,self.columna)
-                print('Semantico',str(error),self.linea,self.columna)
+                self.resultado.add_error('Semantico', str(
+                    error), self.linea, self.columna)
+                print('Semantico', str(error), self.linea, self.columna)
         elif self.tipo == TipoEnum.ARRAY:
-            if len(self.valor.arreglo) == len(result_expresion['value']):
-                try:
-                    scope.declarar_variable(
-                        self.id, result_expresion['value'], self.tipo, self.tipo_secundario, self.linea, self.columna)
-                except ValueError as error:
-                    self.resultado.add_error('Semantico',str(error),self.linea,self.columna)
-                    print('Semantico',str(error),self.linea,self.columna)
+            # Resolvemos el array y verificamos que este completo como al inicio
+            if isinstance(self.valor, Arreglo):
+                if len(self.valor.arreglo) == len(result_expresion['value']):
+                    if self.tipo_secundario == result_expresion['tipo_secundario']:
+                        try:
+                            scope.declarar_variable(
+                                self.id, result_expresion['value'], self.tipo, self.tipo_secundario, self.linea, self.columna)
+                        except ValueError as error:
+                            self.resultado.add_error('Semantico', str(
+                                error), self.linea, self.columna)
+                            print('Semantico', str(error),
+                                  self.linea, self.columna)
+                    else:
+                        self.resultado.add_error(
+                            'Semantico', f'Al crear un array de tipo: {self.tipo_secundario} debe de asignar un array del mismo tipo, esta asignano uno de tipo: {result_expresion["tipo_secundario"]}', self.linea, self.columna)
+                else:
+                    self.resultado.add_error(
+                        'Semantico', 'Hay errores previos antes de la declracion del array', self.linea, self.columna)
             else:
-                self.resultado.add_error(
-                    'Semantico', 'No se declaro el array', self.linea, self.columna)
+                if result_expresion['tipo'] == TipoEnum.ARRAY:
+                    # Validacion de tipos secundarios
+                    if result_expresion['tipo_secundario'] == None:
+                        # Calculo del tipo secundario debido a que viene de un any
+
+                        arreglo = self.calculo_tipo_array(
+                            result_expresion['value'], self.tipo_secundario)
+
+                        if arreglo['tipo_secundario'] == self.tipo_secundario:
+                            scope.declarar_variable(
+                                self.id, result_expresion['value'], self.tipo, self.tipo_secundario, self.linea, self.columna)
+                        else:
+                            error = f'No puede declarar un varaible array de tipo: {self.tipo_secundario} y asignar un tipo: ' + str(
+                                arreglo['tipo_secundario'])
+                            self.resultado.add_error(
+                                'Semantico', error, self.linea, self.columna)
+                            print('Semantico', str(error),
+                                  self.linea, self.columna)
+
+                    elif result_expresion['tipo_secundario'] == self.tipo_secundario:
+
+                        scope.declarar_variable(
+                            self.id, result_expresion['value'], self.tipo, self.tipo_secundario, self.linea, self.columna)
+
+                    else:
+
+                        error = f'No puede declarar un varaible array de tipo: {self.tipo_secundario} y asignar un tipo: ' + str(
+                            result_expresion['tipo_secundario'])
+                        self.resultado.add_error(
+                            'Semantico', error, self.linea, self.columna)
+                        print('Semantico', str(error),
+                              self.linea, self.columna)
+                else:
+                    error = 'No puede declarar un varaible array y asignar un tipo: ' + \
+                        str(result_expresion['tipo'])
+                    self.resultado.add_error(
+                        'Semantico', error, self.linea, self.columna)
+                    print('Semantico', str(error), self.linea, self.columna)
         else:
-            if(result_expresion != None):
+            if (result_expresion != None):
                 tipo: TipoEnum = result_expresion['tipo']
-                #TODO: Verificar por si hay errores mas adelante en la asignacion
+                # TODO: Verificar por si hay errores mas adelante en la asignacion
                 if self.tipo == result_expresion['tipo'] or self.tipo == None:
                     try:
                         scope.declarar_variable(
                             self.id, result_expresion['value'], tipo, None, self.linea, self.columna)
                     except ValueError as error:
-                        self.resultado.add_error('Semantico',str(error),self.linea,self.columna)
-                        print('Semantico',str(error),self.linea,self.columna)
+                        self.resultado.add_error('Semantico', str(
+                            error), self.linea, self.columna)
+                        print('Semantico', str(error),
+                              self.linea, self.columna)
                 else:
                     error = f'No se pude declarar la variable "{self.id}" de tipo : {tipo.value} y asignar un valor tipo: {tipo.value}'
-                    self.resultado.add_error('Semantico', error, self.linea, self.columna)
-                    print('Semantico',str(error),self.linea,self.columna)
+                    self.resultado.add_error(
+                        'Semantico', error, self.linea, self.columna)
+                    print('Semantico', str(error), self.linea, self.columna)
             else:
                 error = f'No se pude declarar la variable "{self.id}" no existe valor para asignar'
-                self.resultado.add_error('Semantico', error, self.linea, self.columna)
-                print('Semantico',str(error),self.linea,self.columna)
+                self.resultado.add_error(
+                    'Semantico', error, self.linea, self.columna)
+                print('Semantico', str(error), self.linea, self.columna)
+
+    def calculo_tipo_array(self, resultado, tipo_variable_contenedora):
+        if len(resultado) == 0:
+            return {"value": [], "tipo": TipoEnum.ARRAY, "tipo_secundario": tipo_variable_contenedora, "linea": self.linea, "columna": self.columna}
+        else:
+            base = resultado[0]['tipo']
+            if all(base == exp['tipo'] for exp in resultado):
+                return {"value": resultado, "tipo": TipoEnum.ARRAY, "tipo_secundario": base.value, "linea": self.linea, "columna": self.columna}
+            else:
+                return {"value": resultado, "tipo": TipoEnum.ARRAY, "tipo_secundario": TipoEnum.ANY.value, "linea": self.linea, "columna": self.columna}
 
     def graficar(self, graphviz, padre):
         graphviz.add_nodo(self.id, padre)
