b6866c3f112914ab625c1622cc53c5ffadf9bff0
cresb-49 <66991258+cresb-49@users.noreply.github.com>
2023-06-13 15:00:33 -0600

    Recuperacion de la tabla de simbolos del programa

--- backend/principal.py
+++ backend/principal.py
@@ -20,15 +20,15 @@ print('#### PARSER FINALIZADO')
 # print(result.sentencias)
 
 ambito_global: Scope = None
-#Buscar si puedo hacer un deep copy sin eliminar lo que ya tengo
-ambitos_copia:Scope = copy.deepcopy(ambito_global)
+# Buscar si puedo hacer un deep copy sin eliminar lo que ya tengo
+ambitos_copia: Scope = copy.deepcopy(ambito_global)
 
 # print('#### ENTORNOS GENERADOS')
 for n in result.tabla_simbolos:
     if isinstance(n, Scope):
         # print('Scope ->', n, ", Anterior ->", n.anterior)
         if n.tipo == 'Global':
-            #ambito_global = copy.deepcopy(n)
+            # ambito_global = copy.deepcopy(n)
             ambito_global = n
         # for x in n.variables.get_diccionario():
             # print('  ', n.variables.get_diccionario()[x])
@@ -66,5 +66,29 @@ for n in result.errores:
 # entorno.graficar(gv,None)
 # print(gv.get_dot())
 
-print('#### ENTORNOS RECUPERADOS')
-print(result.entornos_variables)
+print('#### TABLA DE SIMBOLOS')
+#print(result.entornos_variables)
+
+tabla_de_simbolos = []
+
+#FORMATO PARA INGRESAR LA INFORMACION AL DICCIONARIO
+#{'nombre': value , 'clase': value , 'tipo': value , 'ambito': value ,'fila':value , 'columna': value}
+for key in result.entornos_variables:
+    if result.entornos_variables[key].tipo != 'Global':
+        result.entornos_variables[key].tipo = 'Local'
+    diccionario_vars = result.entornos_variables[key].variables.get_diccionario()
+    diccionario_funciones = result.entornos_variables[key].funciones.get_diccionario()
+    diccionario_estructuras = result.entornos_variables[key].estructuras.get_diccionario()
+    for key2 in diccionario_vars:
+        variable = diccionario_vars[key2] 
+        tabla_de_simbolos.append({'nombre': variable.id , 'clase': 'Variable' , 'tipo': variable.tipo.value , 'ambito': result.entornos_variables[key].tipo ,'linea':variable.linea , 'columna': variable.columna})
+    for key2 in diccionario_funciones:
+        variable = diccionario_funciones[key2] 
+        tabla_de_simbolos.append({'nombre': variable.id , 'clase': 'Funcion' , 'tipo': variable.tipo.value , 'ambito': result.entornos_variables[key].tipo ,'linea':variable.linea , 'columna': variable.columna})
+    for key2 in diccionario_estructuras:
+        variable = diccionario_estructuras[key2] 
+        tabla_de_simbolos.append({'nombre': variable.id , 'clase': 'Estructura' , 'tipo': variable.tipo.value , 'ambito': result.entornos_variables[key].tipo ,'linea':variable.linea , 'columna': variable.columna})
+    
+    
+for simbolos in tabla_de_simbolos:
+    print(simbolos)
