80942dc69cb59033fe17a74e5fca69e56a40352b
cresb-49 <66991258+cresb-49@users.noreply.github.com>
2023-06-12 18:43:41 -0600

    codigo implementar

--- backend/entrada.ts
+++ backend/entrada.ts
@@ -1,17 +1,25 @@
-//Pruebas del print con expreciones artimeticas
-let v: any;
-v = 5;
-//Pruebas de array
-let numeros:any[] = [1, 2, 3, 4, [5,6]];
-let nume = [1, 2, 3, 4, [5,6]];
-console.log(nume);
-//console.log(5[0]);
-let palabras: string[] = ["Hola", "Mundo", "en", "TypeScript"];
-let booleanos: boolean[] = [true, false, true];
-let anys: any[] = ["Hola", false, 3];
+interface Carro {
+    placa: string;
+    color: string;
+    tipo: string;
+};
 
-console.log(palabras[0]);
+let c1: Carro = {
+    placa: "090PLO",
+    color: 'gris',
+    tipo: 'mecanico'
+};
 
-console.log(String(Number("35")));
+let c2: Carro = {
+    placa: "POS921",
+    color: 'verde',
+    tipo: 'automatico'
+};
 
-console.log([1,2,3,[4,5,[6,7,8]]]); // Esta funcion sera extra, la veremos en clase para que la implementen
+//Asignacion de atributos
+c1.color = 'cafe';
+c2.color = 'rojo';
+
+//Acceso Atributo
+console.log(c1.color);
+
