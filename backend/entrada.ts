2c0769d5a09f4961275c527c4dc7b024f87234cd
cresb-49 <66991258+cresb-49@users.noreply.github.com>
2023-06-12 18:48:33 -0600

    actualizacion de la entrada

--- backend/entrada.ts
+++ backend/entrada.ts
@@ -1,21 +1,3 @@
-//Pruebas del print con expreciones artimeticas
-let v: any;
-v = 5;
-//Pruebas de array
-let numeros:any[] = [1, 2, 3, 4, [5,6]];
-let nume = [1, 2, 3, 4, [5,6]];
-console.log(nume);
-//console.log(5[0]);
-let palabras: string[] = ["Hola", "Mundo", "en", "TypeScript"];
-let booleanos: boolean[] = [true, false, true];
-let anys: any[] = ["Hola", false, 3];
-let arrayDeArrays =  [1,2,3,[4,5,[6,[6,[6,[6,7,[6,7,5]],5],5],7,8]]];
-console.log(palabras[0]);
-
-console.log(String(Number("35")), "No se jajaj");
-
-console.log(numeros); // Esta funcion sera extra, la veremos en clase para que la implementen
-
 interface Carro {
     placa: string;
     color: string;
