352c456d0b92fdbe9b2f34921d49c759ea17378d
Luis Monterroso <namenotfound4004@gmail.com>
2023-06-19 20:42:58 -0600

    Terminando cambios en front end c3d

--- frontend/pytypecraft/src/app/components/direcciones-page/direcciones-page.component.ts
+++ frontend/pytypecraft/src/app/components/direcciones-page/direcciones-page.component.ts
@@ -7,8 +7,19 @@ import { EditorComponent } from '../editor/editor.component';
   styleUrls: ['./direcciones-page.component.css'],
 })
 export class DireccionesPageComponent implements AfterViewInit {
+  displayedColumns: string[] = [
+    'tipo',
+    'descripcion',
+    'linea',
+    'columna',
+    'fechaHora',
+  ];
+
+  dataSource = [];
+
   public codigo = '';
   @ViewChild('consoleCode') codeConsole!: EditorComponent;
+  @ViewChild('consoleResult') resultConsole!: EditorComponent;
   /**
    * Al iniciar la vista de codigo debemos comprovar si hay un codigo guardado en las cookies
    * para iniciar con codigo precargado
@@ -29,5 +40,31 @@ export class DireccionesPageComponent implements AfterViewInit {
     let c3d = JSON.parse(localStorage.getItem('compile')!).c3d;
     consola = c3d;
     this.codeConsole.setCode(consola);
+  }
+
+  public sendCode(): void {
+    //Envia el codigo escrito en la consola hacia el backend
+    // this.compileService.sendCode(this.codigo).subscribe((r) => {
+    //   console.log(r);
+    //   localStorage.removeItem('compile');
+    //   localStorage.removeItem('code');
+    //   localStorage.setItem('compile', JSON.stringify(r));
+    //   localStorage.setItem('code', this.codigo);
+    //   this.refreshPage(); //mandamos ha refrescar la pagina
+    // });
+  }
+
+  /**
+   * Este metodo se activa cada vez que el usuairo hace un tecleo en la consola habiitada
+   * @param codigo codigo que escribe el usuario
+   */
+  public setCodigo(codigo: string): void {
+    //eteamos el codigo local con el escrito por el usuario
+    this.codigo = codigo;
+  }
+
+  private setTablaErrores(): void {
+    let errores = JSON.parse(localStorage.getItem('compile')!).errores;
+    this.dataSource = errores;
   }
 }
