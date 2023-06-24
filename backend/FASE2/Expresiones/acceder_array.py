a58faf849a8ab343ac3c50f8a36313c89602218d
cresb-49 <66991258+cresb-49@users.noreply.github.com>
2023-06-23 22:23:39 -0600

    acceso a un array de una dimencion

--- backend/FASE2/Expresiones/acceder_array.py
+++ backend/FASE2/Expresiones/acceder_array.py
@@ -1,12 +1,16 @@
 from FASE2.Abstract.abstract import Abstract
+from FASE2.Abstract.return__ import Return
 from FASE2.Symbol.tipoEnum import TipoEnum
+from FASE2.Symbol.generador import Generador
+from FASE2.Symbol.scope import Scope
+from FASE2.Symbol.Exception import Excepcion
 
 
 class AccederArray(Abstract):
-    def __init__(self, resultado,linea, columna, exprecion, index_exprecion):
+    def __init__(self, resultado,linea, columna, exprecion, list_index_exprecion):
         super().__init__(resultado,linea, columna)
         self.exprecion = exprecion
-        self.index_exprecion = index_exprecion
+        self.list_index_exprecion = list_index_exprecion
 
     def ejecutar(self, scope):
         result = self.exprecion.ejecutar(scope)
@@ -38,5 +42,44 @@ class AccederArray(Abstract):
         else:
             return numero_float
         
-    def generar_c3d(self,scope):
-        pass
+    def generar_c3d(self,scope:Scope):
+        gen_aux = Generador()
+        generador = gen_aux.get_instance()
+        print('DEBUJ ACCEDER ARRAY')
+        # Ejecutamos la exprecion y verificamos que se de tipo ARRAY
+        exprecion:Return = self.exprecion.generar_c3d(scope)
+        if isinstance(exprecion, Excepcion):
+                return exprecion
+        if exprecion.get_tipo() == TipoEnum.ARRAY:
+            if len(self.list_index_exprecion) == 1:
+                index_result:Return = self.list_index_exprecion[0].generar_c3d(scope)
+                if isinstance(index_result,Excepcion): return index_result
+                if index_result.get_tipo() == TipoEnum.NUMBER:
+                    inicio_array_heap = exprecion.get_value()
+                    index = index_result.get_value()
+                    print('DEBUJ ACCEDER INICIO ARRAY: ',exprecion)
+                    size_array = generador.add_temp()
+                    generador.get_heap(size_array,inicio_array_heap)
+                    true_label = generador.new_label()
+                    false_label = generador.new_label()
+                    generador.add_if(index,size_array,'<',true_label)
+                    generador.add_goto(false_label)
+                    # Codigo Para acceder al array
+                    generador.put_label(true_label)
+                    pasos_array = generador.add_temp()
+                    paso = generador.add_temp()
+                    generador.add_exp(paso,index,'1','+')
+                    generador.add_exp(pasos_array,inicio_array_heap,paso,'+')
+                    valor_array = generador.add_temp()
+                    generador.get_heap(valor_array,pasos_array)
+                    generador.put_label(false_label)
+                    # Funcion para que muestre error de parametros de acceso
+                    return Return(valor_array,exprecion.get_tipo_aux(),True,None)
+                else:
+                    self.resultado.add_error('Semantico', 'El valor de acceso para el array debe se un numero', self.linea, self.columna)
+                    return Excepcion('Semantico', 'El valor de acceso para el array debe se un numero', self.linea, self.columna)
+            else:
+                pass
+        else:
+            self.resultado.add_error('Semantico', 'No esta operando un array', self.linea, self.columna)
+            return Excepcion('Semantico', 'No esta operando un array', self.linea, self.columna)
