b6bd7fd15750517f474276024eda5d502f2e0877
cresb-49 <66991258+cresb-49@users.noreply.github.com>
2023-06-26 11:50:04 -0600

    archivos de prueba para cada fase

---
+++ Archivos de prueba/FASE1/interfaces.ts
@@ -0,0 +1,34 @@
+interface Actor {
+    nombre: string;
+    edad: number;
+}
+
+interface Pelicula {
+    nombre: string;
+    posicion: number;
+}
+
+let actores = ["Elizabeth Olsen", "Adam Sandler", "Christian Bale", "Jennifer Aniston"];
+let peliculas = ["Avengers: Age of Ultron", "Mr. Deeds", "Batman: The Dark Knight", "Marley & Me"];
+
+function crearActor(nombre: string, edad: number): any {
+    return {nombre: nombre, edad: edad};
+}
+
+function crearPelicula(nombre: string, posicion: number): any {
+    return { nombre: nombre, posicion: posicion };
+}
+function imprimir(actor:any,pelicula:any) {
+	console.log('Nombre: ',actor.nombre,', Edad: ',actor.edad);
+	console.log('Nombre: ',pelicula.nombre,', Posicion: ',pelicula.posicion);
+}
+function contratos() {
+    for (let i = 1; i < 3; i++) {
+        if (i < 4) {
+            let actor = crearActor(actores[i - 1], i + 38);
+            let pelicula = crearPelicula(peliculas[i - 1], i);
+            imprimir(actor,pelicula);
+        }
+    }
+}
+contratos();
